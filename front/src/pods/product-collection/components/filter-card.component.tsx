468c8a1f1dee080b73eece4ba3164c7c9c707d44
olivenze <eduardolivenza@gmail.com>
2019-11-18 10:40:25 +0100

    Modify filter card

--- front/src/pods/product-collection/components/filter-card.component.tsx
+++ front/src/pods/product-collection/components/filter-card.component.tsx
@@ -1,26 +1,32 @@
 import * as React from "react";
 import Card from "@material-ui/core/Card";
-import { makeStyles } from '@material-ui/core/styles';
-import { CardContent, CardHeader, TextField, } from "@material-ui/core";
-import { CheckBoxConfigValue, CheckBoxGroup, TextAreaForm } from "common/components";
-import Typography from '@material-ui/core/Typography';
-import Slider from '@material-ui/core/Slider';
+import { makeStyles } from "@material-ui/core/styles";
+import { CardContent, CardHeader, TextField } from "@material-ui/core";
+import {
+  CheckBoxConfigValue,
+  CheckBoxGroup,
+  TextAreaForm
+} from "common/components";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles(theme => ({
-  card: {
-    textAlign: "left",
-    marginTop: theme.spacing(1),
-    marginRight: theme.spacing(1),
-  },
   priceNameFilter: {
-    width: 300,
+    width: 400
   },
   textField: {
     marginRight: theme.spacing(1),
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2)
   },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightBold
+  }
 }));
-
 
 interface Props {
   handleChangeComfortFilter: (name: string, value: boolean) => void;
@@ -29,9 +35,9 @@ interface Props {
   handleChangeFilterText: (name: string) => void;
   comfortLevelFilterState: CheckBoxConfigValue[];
   productTypesFilterState: CheckBoxConfigValue[];
-  filterTextValue: string,
-  maxPriceValue: number,
-  selectedPrice: number[],
+  filterTextValue: string;
+  maxPriceValue: number;
+  selectedPrice: number[];
 }
 
 function valuetext(value) {
@@ -39,7 +45,6 @@ function valuetext(value) {
 }
 
 export const FilterCard = (props: Props) => {
-
   const classes = useStyles({});
 
   const {
@@ -51,27 +56,35 @@ export const FilterCard = (props: Props) => {
     selectedPrice,
     maxPriceValue,
     filterTextValue,
-    handleChangePriceFilter } = props;
+    handleChangePriceFilter
+  } = props;
 
   const handleChange = (event, newValue: number[]) => {
     handleChangePriceFilter(newValue);
   };
 
-  const handleChangeText = (event) => {
+  const handleChangeText = event => {
     handleChangeFilterText(event.target.value);
   };
 
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        title="Products filter"
-      />
-      <CardContent>
-        <div style={{
-          display: "flex",
-          justifyContent: 'space-between',
-          flexWrap: "wrap",
-        }}>
+    <ExpansionPanel>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography className={classes.heading}>Produ products</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <div
+          style={{
+            flexBasis: "60%",
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap"
+          }}
+        >
           <CheckBoxGroup
             label={"Filter by comfort level"}
             handleChangeCheckbox={handleChangeComfortFilter}
@@ -107,8 +120,7 @@ export const FilterCard = (props: Props) => {
             />
           </div>
         </div>
-      </CardContent>
-    </Card>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   );
 };
-
